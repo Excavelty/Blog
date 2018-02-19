dd1c20d882d03b96d183e258039decad34e372bf
Excavelty <gambitek2@gmail.com>
2018-02-19 17:28:12 +0100

    Update config.php

--- lib/config.php
+++ lib/config.php
@@ -1,8 +1,5 @@
 <?php
 class Config{
-    public static $dsn = "mysql:host=localhost;dbname=blog";
-    public static $login = 'root';
-    public static $password = '';
     
     public static function getPDO($dsn = "mysql:host=localhost;dbname=blog", $login = 'root', $password = ''){
         $pdo = new PDO($dsn, $login, $password);
